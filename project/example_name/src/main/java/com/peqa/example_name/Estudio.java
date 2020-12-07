a19b079e1abf3269639f452620d7aac2319e087c
Erick Agrazal <erick.agrazal@gmail.com>
2020-12-06 19:11:33 -0500

    Added estudios.

---
+++ project/example_name/src/main/java/com/peqa/example_name/Estudio.java
@@ -0,0 +1,21 @@
+package com.peqa.example_name;
+
+
+public class Estudio {
+    String institucion;
+    String titulo;
+
+    public void setInstitucion(String _institucion){
+        institucion = _institucion;
+    }
+    public String getInstitucion() {
+        return institucion;
+    }
+
+    public void setTitulo(String _titulo){
+        titulo = _titulo;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+}
